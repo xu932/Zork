624e4fbf9f7146ac31edf77b35bf616a1efa5591
Alex <xu932@purdue.edu>
2019-03-17 16:59:49 -0400

    added utility function

---
+++ Headers/UtilityFunctions.h
@@ -0,0 +1,16 @@
+//
+// Created by alex on 3/17/19.
+//
+
+#ifndef ZORK_UTILITYFUNCTIONS_H
+#define ZORK_UTILITYFUNCTIONS_H
+
+#include <vector>
+#include <iostream>
+#include <string>
+#include <unordered_map>
+
+void parseString(std::string str, std::vector<std::string>& parse);
+int readCommand(std::string command, std::vector<std::string>& parse);
+
+#endif //ZORK_UTILITYFUNCTIONS_H
---
+++ Utility/UtilityFunctions.cpp
@@ -0,0 +1,43 @@
+//
+// Created by alex on 3/17/19.
+//
+#include "../Headers/UtilityFunctions.h"
+
+void parseString(std::string str, std::vector<std::string>& parse) {
+    int prev = 0;
+    parse.clear();
+    for (unsigned int i = 1; i < str.length(); i++) {
+        if (str[i] != ' ')  continue;
+        parse.push_back(str.substr(prev, i - prev));
+        prev = i + 1;
+    }
+    parse.push_back(str.substr(prev));
+}
+
+int readCommand(std::string command, std::vector<std::string>& parse) {
+    parseString(command, parse);
+    int size = parse.size();
+
+    if (parse[0] == "n" || parse[0] == "s" || parse[0] == "e" || parse[0] == "w")   return 1;
+    else if (parse[0] == "i" || parse[0] == "inventory")                            return 2;
+    else if (parse[0] == "take")                                                    return 3;
+    else if (parse[0] == "open") {
+        if (size >= 2 && parse[1] != "exit")                                        return 4;
+        else if (size >= 2)                                                         return 5;
+        else return 0;
+    } else if (parse[0] == "read" && size >= 2)                                     return 6;
+    else if (parse[0] == "drop" && size >= 2)                                       return 7;
+    else if (parse[0] == "put") {
+        for (int i = 1; i < size - 1; i++) {
+            if (parse[i] == "in")                                                   return 8;
+        }
+        return 0;
+    } else if (parse[0] == "turn" && size >= 3 && parse[1] == "on")                 return 9;
+    else if (parse[0] == "attack") {
+        for (int i = 1; i < size - 1; i++) {
+            if (parse[i] == "with")                                                 return 10;
+        }
+        return 0;
+    } else
+        return 0;
+}
