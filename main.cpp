828b81456d0747be0ecdcdd8d830437a6265f702
Alex <xu932@purdue.edu>
2019-03-17 21:25:27 -0400

    added trigger checking when running game

--- main.cpp
+++ main.cpp
@@ -8,6 +8,7 @@
 #include "Headers/rapidxml.hpp"
 #include "Headers/rapidxml_utils.hpp"
 #include "Headers/Map.h"
+#include "Headers/UtilityFunctions.h"
 
 std::unordered_map<std::string, std::vector<rapidxml::xml_node<>*>> extractElements(rapidxml::xml_node<>* root) {
     std::unordered_map<std::string, std::vector<rapidxml::xml_node<>*>> elements;
@@ -26,6 +27,61 @@ std::unordered_map<std::string, std::vector<rapidxml::xml_node<>*>> extractEleme
     return elements;
 }
 
+void runZork(Map* map) {
+    std::shared_ptr<Container> inventory = std::make_shared<Container>();   // player's inventory
+    std::string command;                                                    // for user input command
+    std::vector<std::string> parse;                                         // for parsed command
+    int type;                                                               // to decode which command it is
+
+    map->initTriggers(inventory);
+
+    std::shared_ptr<Room> current = map->getRoom("Entrance");               // the current room
+    current->init();
+
+    while (map->isRunning()) {
+        auto trig = current->checkTrigger("");
+        if (trig != nullptr) {
+            trig->fire();
+            for (auto s : trig->actions)
+                map->executeAction(s);
+            continue;
+        }
+
+        trig = inventory->checkTrigger("");
+        if (trig != nullptr) {
+            trig->fire();
+            for (auto s : trig->actions)
+                map->executeAction(s);
+            continue;
+        }
+
+        type = 0;
+        do {
+            std::getline(std::cin, command);
+            type = readCommand(command, parse);
+            if (type == 0)
+                std::cerr << "Error" << std::endl;
+        } while (type == 0);
+
+        trig = current->checkTrigger(command);
+        if (trig != nullptr) {
+            trig->fire();
+            for (auto s : trig->actions)
+                map->executeAction(s);
+            continue;
+        }
+
+        trig = inventory->checkTrigger(command);
+        if (trig != nullptr) {
+            trig->fire();
+            for (auto s : trig->actions)
+                map->executeAction(s);
+            continue;
+        }
+    }
+
+}
+
 int main(int argc, char ** argv) {
     rapidxml::file<> xmlFile(argv[1]);
     rapidxml::xml_document<> doc;
@@ -34,6 +90,7 @@ int main(int argc, char ** argv) {
     auto elements = extractElements(doc.first_node());
 
     Map *map = new Map(elements);
+    runZork(map);
 
     delete map;
     return EXIT_SUCCESS;
